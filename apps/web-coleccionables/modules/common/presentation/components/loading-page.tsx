514659981083a1b580bcc48ce3f3369f8e6321a9
Gountzjs <tomasjsdev@gmail.com>
2025-12-10 02:34:42 -0300

    feat(web-coleccionables): :sparkles: create loading page component

---
+++ apps/web-coleccionables/modules/common/presentation/components/loading-page.tsx
@@ -0,0 +1,7 @@
+export function LoadingPage() {
+	return (
+		<div className="flex items-center gap-4 justify-center w-full min-h-[calc(100vh-75px)] px-mobile md:px-tablet lg:px-desktop">
+			Loading...
+		</div>
+	);
+}
