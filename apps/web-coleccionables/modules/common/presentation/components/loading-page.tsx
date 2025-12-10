f553207f518a2e21c3b0697903687bde36578c7d
Gountzjs <tomasjsdev@gmail.com>
2025-12-10 04:03:06 -0300

    feat(web-coleccionables): :sparkles: loading-page cambiar calculo min height

--- apps/web-coleccionables/modules/common/presentation/components/loading-page.tsx
+++ apps/web-coleccionables/modules/common/presentation/components/loading-page.tsx
@@ -1,6 +1,6 @@
 export function LoadingPage() {
 	return (
-		<div className="flex items-center gap-4 justify-center w-full min-h-[calc(100vh-75px)] px-mobile md:px-tablet lg:px-desktop">
+		<div className="flex items-center gap-4 justify-center w-full min-h-[calc(100vh-85px)] px-mobile md:px-tablet lg:px-desktop">
 			Loading...
 		</div>
 	);
