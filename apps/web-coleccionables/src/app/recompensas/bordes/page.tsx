80590c22b0b17b2c10d0bf707c943139a8fb1312
Gountzjs <tomasjsdev@gmail.com>
2025-12-10 07:00:20 -0300

    feat(web-coleccionables): :sparkles: integrate listclasification bordersprice recompensas bordes sections

--- apps/web-coleccionables/src/app/recompensas/bordes/page.tsx
+++ apps/web-coleccionables/src/app/recompensas/bordes/page.tsx
@@ -1,4 +1,6 @@
 import { Disclaimer } from "@modules/common/presentation/components/disclaimer";
+import { BordersPrice } from "./sections/borders-price";
+import { ListClasification } from "./sections/list-clasification";
 import { Presentation } from "./sections/presentation";
 
 export default function Page() {
@@ -6,6 +8,8 @@ export default function Page() {
 		<main className="flex flex-col items-center pb-6 w-full min-h-[calc(100vh-75px)] px-mobile md:px-tablet lg:px-desktop">
 			<Presentation />
 			<Disclaimer />
+			<ListClasification />
+			<BordersPrice />
 		</main>
 	);
 }
