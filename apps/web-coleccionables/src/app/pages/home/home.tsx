73f6175733919a6c8199cf070a70083024a3f62c
Gountzjs <tomasjsdev@gmail.com>
2025-12-10 02:30:29 -0300

    feat(web-coleccionables): :sparkles: integrate slider and sections into home page

--- apps/web-coleccionables/src/app/pages/home/home.tsx
+++ apps/web-coleccionables/src/app/pages/home/home.tsx
@@ -1,7 +1,27 @@
+import * as Slider from "@modules/common/presentation/lib/slider";
+import { EmbeeBorders } from "./_sections/EmbeeBorders";
+import { EmbeeCards } from "./_sections/EmbeeCards";
+import { Presentation } from "./_sections/Presentation";
+
 export default function Page() {
 	return (
-		<main className="flex items-center justify-center w-full h-[calc(100vh-75px)]">
-			<h1>Hello Home!!</h1>
+		<main className="flex items-center gap-4 justify-center w-full min-h-[calc(100vh-75px)] px-mobile md:px-tablet lg:px-desktop">
+			<Slider.Provider>
+				<Slider.Box>
+					<Slider.Item key="0">
+						<Presentation />
+					</Slider.Item>
+
+					<Slider.Item key="1">
+						<EmbeeBorders />
+					</Slider.Item>
+
+					<Slider.Item key="2">
+						<EmbeeCards />
+					</Slider.Item>
+					<Slider.Dots length={3} />
+				</Slider.Box>
+			</Slider.Provider>
 		</main>
 	);
 }
