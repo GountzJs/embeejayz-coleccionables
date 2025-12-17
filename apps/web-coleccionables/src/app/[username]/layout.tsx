3330b201a9511d34aa2006018eed65f0c454d7d1
Gountzjs <tomasjsdev@gmail.com>
2025-12-17 03:48:49 -0300

    feat(web-coleccionables): :sparkles: create and integrate tabs user layout

--- apps/web-coleccionables/src/app/[username]/layout.tsx
+++ apps/web-coleccionables/src/app/[username]/layout.tsx
@@ -1,5 +1,8 @@
-import { ProfileProvider } from "@modules/accounts/presentation/contexts/profile.context";
-import { Navigate, Outlet, useParams } from "react-router";
+import {
+	ProfileProvider,
+	useProfileContext,
+} from "@modules/accounts/presentation/contexts/profile.context";
+import { Link, Navigate, Outlet, useLocation, useParams } from "react-router";
 
 export function UserLayout() {
 	const { username = "" } = useParams();
@@ -15,12 +18,77 @@ export function UserLayout() {
 					<p>My perfil</p>
 				</aside>
 				<section className="flex flex-col gap-6 flex-1 h-full w-full">
-					<nav>tabs</nav>
+					<Tabs />
 					<article className="h-full w-full">
 						<Outlet />
 					</article>
 				</section>
 			</ProfileProvider>
 		</main>
+	);
+}
+
+function Tabs() {
+	const { username } = useParams();
+	const { isLoading, data: profile } = useProfileContext();
+	const { pathname: path } = useLocation();
+
+	const isActive = (href: string) => path === href;
+
+	if (isLoading || !profile)
+		return (
+			<nav className="flex items-end gap-4 w-full border-b border-b-gray-light/20 opacity-50 py-1">
+				<span
+					className={`font-semibold transition-colors duration-300 underline-red-500 decoration-3 underline-offset-8 ${
+						isActive(`/usuarios/${username}`) ? "text-red-500" : "text-white"
+					}`}
+				>
+					Bordes
+				</span>
+				<span
+					className={`font-semibold transition-colors duration-300 underline-red-500 decoration-3 underline-offset-8 ${
+						isActive(`/usuarios/${username}/embeecards`)
+							? "text-red-500"
+							: "text-white"
+					}`}
+				>
+					Cartas
+				</span>
+				<span
+					className={`font-semibold transition-colors duration-300 underline-red-500 decoration-3 underline-offset-8 ${
+						isActive(`/usuarios/${username}/worlds2025`)
+							? "text-red-500"
+							: "text-white"
+					}`}
+				>
+					Álbum Worlds 2025
+				</span>
+			</nav>
+		);
+
+	return (
+		<nav className="flex items-end gap-4 w-full border-b border-b-gray-light/20 py-1">
+			<TabLink href={`/usuarios/${username}`}>Bordes</TabLink>
+			<TabLink href={`/usuarios/${username}/embeecards`}>Cartas</TabLink>
+			<TabLink href={`/usuarios/${username}/worlds2025`}>
+				Álbum Worlds 2025
+			</TabLink>
+		</nav>
+	);
+}
+
+function TabLink({ href, children }: { href: string; children: string }) {
+	const { pathname } = useLocation();
+	const isActive = pathname === href;
+
+	return (
+		<Link
+			to={href}
+			className={`font-semibold transition-colors duration-300 hover:text-red-500 underline-red-500 decoration-3 underline-offset-8 ${
+				isActive ? "text-red-500" : "text-white hover:underline "
+			}`}
+		>
+			{children}
+		</Link>
 	);
 }
