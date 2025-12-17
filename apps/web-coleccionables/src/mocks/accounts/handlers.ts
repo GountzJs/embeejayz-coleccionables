cd9dcd17c810d6996d29309b9018f9680f84189c
Gountzjs <tomasjsdev@gmail.com>
2025-12-17 03:05:49 -0300

    feat(web-coleccionables): :sparkles: mock handlers account add endpoint get profile

--- apps/web-coleccionables/src/mocks/accounts/handlers.ts
+++ apps/web-coleccionables/src/mocks/accounts/handlers.ts
@@ -7,4 +7,19 @@ export const handlers = [
 			data: rankingData,
 		});
 	}),
+	http.get("http://localhost:4200/api/accounts/:username", () => {
+		return HttpResponse.json({
+			data: {
+				id: "055b8c07-1839-48d7-977b-48e85b58bfbd",
+				avatar:
+					"https://static-cdn.jtvnw.net/jtv_user_pictures/acec5321-f291-4f97-91c3-f62498d68dbd-profile_image-300x300.png",
+				username: "Meldrekoski",
+				total: {
+					borders: 675,
+					cards: 100,
+					general: 775,
+				},
+			},
+		});
+	}),
 ];
