070819126a60dc7474aaa0d31bb3107af2ad038f
Gountzjs <tomasjsdev@gmail.com>
2025-12-18 10:35:25 -0300

    feat(web-coleccionables): :lipstick: modify align listborderskeleton

--- apps/web-coleccionables/modules/borders/presentation/components/list-border-skeleton.tsx
+++ apps/web-coleccionables/modules/borders/presentation/components/list-border-skeleton.tsx
@@ -2,7 +2,7 @@ import { SkeletonBorder } from "./skeleton-border";
 
 export function ListBorderSkeleton() {
 	return (
-		<ul className="flex flex-wrap justify-center gap-4 w-full max-w-full">
+		<ul className="flex flex-wrap gap-4 w-full max-w-full">
 			{Array.from({ length: 10 }).map((_, idx) => (
 				// biome-ignore lint/suspicious/noArrayIndexKey: <Migrate by preact and eslint>
 				<li key={idx}>
