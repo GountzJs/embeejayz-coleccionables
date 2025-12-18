68bd2e12f701a3628b47992d07b23f1826fc6ccf
Gountzjs <tomasjsdev@gmail.com>
2025-12-18 10:39:14 -0300

    feat(web-coleccionables): :sparkles: borders presentation create list borders component

---
+++ apps/web-coleccionables/modules/borders/presentation/components/list-borders.tsx
@@ -0,0 +1,69 @@
+/** biome-ignore-all lint/suspicious/noArrayIndexKey: false positive */
+import { spells } from "@modules/borders/domain/consts/spells.const";
+import { InfiniteScrollObserver } from "@modules/common/presentation/lib/infinite-scroll";
+import { useGetBorderFilters } from "../hooks/get-border-filters";
+import { useGetBordersPaginationQuery } from "../queries/get-border-pagination.query";
+import { getRankByQuantity } from "../utils/rank";
+import { BorderRank } from "./border-rank";
+import { NotBorders } from "./not-borders";
+import { SkeletonBorder } from "./skeleton-border";
+
+interface Props {
+	id: string;
+	avatarUrl: string;
+
+	username: string;
+}
+
+export function ListBorders({ id, avatarUrl, username }: Props) {
+	const { orderBy, sort, name } = useGetBorderFilters();
+	const {
+		borders,
+		isFetching,
+		isFetchingNextPage,
+		hasNextPage,
+		fetchNextPage,
+	} = useGetBordersPaginationQuery({
+		id,
+		orderBy,
+		sort,
+		name,
+	});
+
+	if (!borders.length && name.length)
+		return (
+			<p className="text-2xl font-medium text-center">
+				No se encontró ningun borde con el nombre buscado, intente con otro.
+			</p>
+		);
+
+	if (!borders.length) return <NotBorders />;
+
+	return (
+		<ul className="flex flex-wrap gap-4 h-full w-full max-w-full">
+			{borders.map((border, idx) => (
+				<li key={`${border.name}-${idx}`}>
+					<BorderRank
+						border={border}
+						username={username}
+						avatarUrl={avatarUrl}
+						rank={getRankByQuantity(border.quantity, border.isSpecial)}
+						leftIcon={spells.flash}
+						rightIcon={spells.ignite}
+					/>
+				</li>
+			))}
+			{(isFetching || isFetchingNextPage) &&
+				Array.from({ length: 10 }).map((_, idx) => (
+					<SkeletonBorder key={`skeleton-${idx}`} />
+				))}
+
+			{hasNextPage && (
+				<InfiniteScrollObserver
+					onIntersect={fetchNextPage}
+					isLoading={isFetching}
+				/>
+			)}
+		</ul>
+	);
+}
