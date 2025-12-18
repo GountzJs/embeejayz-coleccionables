120f6dfc0f64a1f21705f585059cc7eb0e5302c4
Gountzjs <tomasjsdev@gmail.com>
2025-12-18 10:42:11 -0300

    feat(web-coleccionables): :sparkles: username page integrate filter borders and list borders

--- apps/web-coleccionables/src/app/[username]/page.tsx
+++ apps/web-coleccionables/src/app/[username]/page.tsx
@@ -1,3 +1,20 @@
+import { useProfileContext } from "@modules/accounts/presentation/contexts/profile.context";
+import { ListBorders } from "@modules/borders/presentation/components/list-borders";
+import { FilterBorders } from "./sections/filter-borders";
+
 export default function Page() {
-	return <div>Borders page</div>;
+	const { data } = useProfileContext();
+
+	return (
+		<div className="flex flex-col flex-1 gap-14 w-full h-full">
+			<FilterBorders />
+			{data && (
+				<ListBorders
+					id={data.id}
+					avatarUrl={data.avatar}
+					username={data.username}
+				/>
+			)}
+		</div>
+	);
 }
