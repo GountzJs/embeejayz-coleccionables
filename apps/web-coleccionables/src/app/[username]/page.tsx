aac572a32a653d9c7ceba5db4492785b5ca9b1a4
Gountzjs <tomasjsdev@gmail.com>
2025-12-18 11:30:11 -0300

    feat(web-coleccionables): :sparkles: username page integrate spells context

--- apps/web-coleccionables/src/app/[username]/page.tsx
+++ apps/web-coleccionables/src/app/[username]/page.tsx
@@ -1,5 +1,7 @@
 import { useProfileContext } from "@modules/accounts/presentation/contexts/profile.context";
 import { ListBorders } from "@modules/borders/presentation/components/list-borders";
+import { DialogSpellContextProvider } from "@modules/borders/presentation/contexts/dialog-spell.context";
+import { SpellsContextProvider } from "@modules/borders/presentation/contexts/spells.context";
 import { FilterBorders } from "./sections/filter-borders";
 
 export default function Page() {
@@ -7,14 +9,18 @@ export default function Page() {
 
 	return (
 		<div className="flex flex-col flex-1 gap-14 w-full h-full">
-			<FilterBorders />
-			{data && (
-				<ListBorders
-					id={data.id}
-					avatarUrl={data.avatar}
-					username={data.username}
-				/>
-			)}
+			<SpellsContextProvider>
+				<DialogSpellContextProvider>
+					<FilterBorders />
+					{data && (
+						<ListBorders
+							id={data.id}
+							avatarUrl={data.avatar}
+							username={data.username}
+						/>
+					)}
+				</DialogSpellContextProvider>
+			</SpellsContextProvider>
 		</div>
 	);
 }
