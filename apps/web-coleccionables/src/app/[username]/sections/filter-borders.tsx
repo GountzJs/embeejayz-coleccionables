d7b18bbf48e7248157da2c781c7a97d988b3aea8
Gountzjs <tomasjsdev@gmail.com>
2025-12-18 11:30:40 -0300

    feat(web-coleccionables): :sparkles: username sections filter borders integrate changespell and dialog spell

--- apps/web-coleccionables/src/app/[username]/sections/filter-borders.tsx
+++ apps/web-coleccionables/src/app/[username]/sections/filter-borders.tsx
@@ -1,3 +1,5 @@
+import { ChangeSpells } from "@modules/borders/presentation/components/change-spells/change-spells";
+import { DialogSpell } from "@modules/borders/presentation/components/dialog-spell/dialog-spell";
 import { SearchBorders } from "@modules/borders/presentation/components/search-borders/search-borders";
 import { useGetBorderFilters } from "@modules/borders/presentation/hooks/get-border-filters";
 
@@ -7,6 +9,8 @@ export function FilterBorders() {
 	return (
 		<div className="flex items-center gap-4 w-full">
 			<SearchBorders sort={sort} orderBy={orderBy} name={name} />
+			<ChangeSpells />
+			<DialogSpell />
 		</div>
 	);
 }
