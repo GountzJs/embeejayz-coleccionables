1c9dfc8da7d5575da53114fe6155ecce423202b7
Gountzjs <tomasjsdev@gmail.com>
2025-12-18 00:34:24 -0300

    feat(web-coleccionables): :sparkles: borders domain consts modify types borderorderby and bordersort

--- apps/web-coleccionables/modules/borders/domain/consts/borders.const.ts
+++ apps/web-coleccionables/modules/borders/domain/consts/borders.const.ts
@@ -3,11 +3,12 @@ export const bordersOrderBy = {
 	createdAt: "createdAt",
 } as const;
 
-export type TBordersOrderBy = keyof typeof bordersOrderBy;
+export type TBordersOrderBy =
+	(typeof bordersOrderBy)[keyof typeof bordersOrderBy];
 
 export const borderSort = {
 	asc: "ASC",
 	desc: "DESC",
 } as const;
 
-export type TBorderSort = keyof typeof borderSort;
+export type TBorderSort = (typeof borderSort)[keyof typeof borderSort];
