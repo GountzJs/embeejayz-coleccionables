050279f607d1d1a7f79692f7f0bd012bf7a5d079
Gountzjs <tomasjsdev@gmail.com>
2025-12-17 21:36:30 -0300

    feat(web-coleccionables): :sparkles: borders presentation create skeleton border and list border skeleton

---
+++ apps/web-coleccionables/modules/borders/presentation/components/list-border-skeleton.tsx
@@ -0,0 +1,14 @@
+import { SkeletonBorder } from "./skeleton-border";
+
+export function ListBorderSkeleton() {
+	return (
+		<ul className="flex flex-wrap justify-center gap-4 w-full max-w-full">
+			{Array.from({ length: 10 }).map((_, idx) => (
+				// biome-ignore lint/suspicious/noArrayIndexKey: <Migrate by preact and eslint>
+				<li key={idx}>
+					<SkeletonBorder />
+				</li>
+			))}
+		</ul>
+	);
+}
