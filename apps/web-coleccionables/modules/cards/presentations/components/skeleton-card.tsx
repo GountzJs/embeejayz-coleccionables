f27fa9f31a5008f3e4eb32d0dca116419185db3c
Gountzjs <tomasjsdev@gmail.com>
2025-12-19 18:11:08 -0300

    fix(web-coleccionables): :lipstick: cards presentation component skeletoncard adapt size

--- apps/web-coleccionables/modules/cards/presentations/components/skeleton-card.tsx
+++ apps/web-coleccionables/modules/cards/presentations/components/skeleton-card.tsx
@@ -1,7 +1,7 @@
 export function SkeletonCard() {
 	return (
 		<div
-			className="rounded-3xl w-[350px] h-[450px] animate-bgfade"
+			className="rounded-3xl w-full h-full animate-bgfade"
 			style={{
 				background:
 					"linear-gradient(270deg,oklch(55.1% 0.027 264.364),oklch(44.6% 0.03 256.802),oklch(37.3% 0.034 259.733))",
