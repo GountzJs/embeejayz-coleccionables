8c63747e55179c5cc74d0cbec99cf81328714aa7
Gountzjs <tomasjsdev@gmail.com>
2025-12-20 18:05:37 -0300

    feat(gountz-bot): :recycle: integrate dotenv managment env files

--- apps/gountz-bot/src/core/settings.ts
+++ apps/gountz-bot/src/core/settings.ts
@@ -1,9 +1,11 @@
+// biome-ignore assist/source/organizeImports: <Autosave import>
+import { config } from "dotenv";
 import { resolve } from "node:path";
 import { cwd } from "node:process";
 
 if (process.env.NODE_ENV === "development") {
-	const envPath = resolve(cwd(), ".env");
-	process.loadEnvFile(envPath);
+	const envPath = resolve(cwd(), ".env.local");
+	config({ path: envPath });
 }
 
 const env = process.env;
