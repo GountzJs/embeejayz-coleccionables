0bbbf7c1f3788dad73be213a9f02207d01d2d028
Gountzjs <tomasjsdev@gmail.com>
2025-12-21 15:43:54 -0300

    feat(web-coleccionables): :sparkles: integrate ui-card package

--- apps/web-coleccionables/modules/cards/presentations/components/card-element.tsx
+++ apps/web-coleccionables/modules/cards/presentations/components/card-element.tsx
@@ -1,23 +1,26 @@
+// biome-ignore assist/source/organizeImports: <Autosave import>
+import { cdnUrl } from "@/core/settings";
 import { type EmbeecardEntity, embeecardCategory } from "@embeejayz/core-cards";
-
-import { CardEmbee } from "../lib/templates/card-embee";
-import { CardLolesports } from "../lib/templates/card-lolesports";
-import { CardSpecial } from "../lib/templates/card-special";
-import { CardT1 } from "../lib/templates/card-t1";
-import { CardTroll } from "../lib/templates/card-troll";
+import {
+	CardEmbee,
+	CardLolesports,
+	CardSpecial,
+	CardT1,
+	CardTroll,
+} from "@embeejayz/ui-cards";
 
 const CardComponent = (props: EmbeecardEntity) => {
 	switch (props.category) {
 		case embeecardCategory.lolesports:
-			return <CardLolesports {...props} />;
+			return <CardLolesports {...props} cdnUrl={cdnUrl} />;
 		case embeecardCategory.embee:
-			return <CardEmbee {...props} />;
+			return <CardEmbee {...props} cdnUrl={cdnUrl} />;
 		case embeecardCategory.troll:
-			return <CardTroll {...props} />;
+			return <CardTroll {...props} cdnUrl={cdnUrl} />;
 		case embeecardCategory.t1:
-			return <CardT1 {...props} />;
+			return <CardT1 {...props} cdnUrl={cdnUrl} />;
 		default:
-			return <CardSpecial {...props} />;
+			return <CardSpecial {...props} cdnUrl={cdnUrl} />;
 	}
 };
 
