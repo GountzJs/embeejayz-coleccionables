cd210bc6e449c231016cd6d5a037d94c91b9dfeb
Gountzjs <tomasjsdev@gmail.com>
2025-12-30 09:30:05 -0300

    feat(overlay-notifications): :sparkles: change name environemtns width public word

--- apps/overlay-notifications/src/consts/settings.ts
+++ apps/overlay-notifications/src/consts/settings.ts
@@ -1,9 +1,9 @@
 const env = import.meta.env;
 
 export const {
-	VITE_TWITCH_CHANNEL: twitchChannel = "",
-	VITE_CDN_URL: cdnUrl = "",
-	VITE_API_URL: apiUrl = "",
+	VITE_PUBLIC_TWITCH_CHANNEL: twitchChannel = "",
+	VITE_PUBLIC_CDN_URL: cdnUrl = "",
+	VITE_PUBLIC_API_URL: apiUrl = "",
 } = env;
 
 export const debug = (env.VITE_DEBUG ?? "false") === "true";
